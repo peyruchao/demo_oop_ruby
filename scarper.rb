03e778356130778ed7bd04bf8fc66a79650c0286
ethancychen <gatheringbc@yahoo.com.tw>
2014-10-09 14:38:14 +0800

    v1

--- scarper.rb
+++ scarper.rb
@@ -7,25 +7,11 @@ module Scarper
 		LINK = 'http://www.thenewslens.com'
 		NEWS_XPATH = "//main[@class='HolyGrail-content']//div[@class='post-list-item']"
 
-		def self.getNews
-			doc = getHtml(LINK)
-			content = getContent(doc)
-			titles = getTitles(content)
-		end
 		def self.getHtml(url)			
 			doc = Nokogiri::HTML(open(url))
 			#doc
 		end
 
-		
-		
-		def self.getContent(doc)			
-			news = doc.xpath(NEWS_XPATH)
-		end
-
-		def self.getTitles(news)
-			news.map{|x| x.text}
-		end
 
 	end
 end
