29cb0e5070cfc031a2e25e7bf316e5db8978d8f8
peggy <peggy@140-127-218-233.nuk.edu.tw>
2014-10-09 17:54:01 +0800

    v3

--- scarpers.rb
+++ scarpers.rb
@@ -5,13 +5,19 @@ module Scarper
 	
 	class NewsLens
 		LINK = 'http://www.thenewslens.com'
+		NEWS_XPATH = "//main[@class='HolyGrail-content']//div[@class='post-list-item']"
+
+		def self.getNews
+			doc = getHtml(LINK)
+			content = getContent(doc)
+			titles = getTitles(content)
+		end
+
 		def self.getHtml(url)			
 			doc = Nokogiri::HTML(open(url))
 			#doc
 		end
 
-		
-		
 		def self.getContent(doc)			
 			news = doc.xpath(NEWS_XPATH)
 		end
