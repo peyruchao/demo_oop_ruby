d5ba1b1dd1a3e686b692973e63b4e14c08df06be
peggy <peggy@140-127-218-233.nuk.edu.tw>
2014-10-12 15:40:27 +0800

    v4(after rubocop)

--- newsdigest.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'scarpers.rb'
-
-newsfound = Scarper::NewsLens.getNews
-
-newsfound.each do |x|
-  puts x
-end
--- scarpers.rb
+++ scarpers.rb
@@ -1,30 +1,22 @@
 require 'nokogiri'
 require 'open-uri'
-
 module Scarper
-	
-	class NewsLens
-		LINK = 'http://www.thenewslens.com'
-		NEWS_XPATH = "//main[@class='HolyGrail-content']//div[@class='post-list-item']"
-
-		def self.getNews
-			doc = getHtml(LINK)
-			content = getContent(doc)
-			titles = getTitles(content)
-		end
-
-		def self.getHtml(url)			
-			doc = Nokogiri::HTML(open(url))
-			#doc
-		end
-
-		def self.getContent(doc)			
-			news = doc.xpath(NEWS_XPATH)
-		end
-
-		def self.getTitles(news)
-			news.map{|x| x.text}
-		end
-
-	end
+  class NewsLens
+    LINK = 'http://www.thenewslens.com'
+    NEWS_XPATH = "//main[@class='HolyGrail-content']//div[@class='post-list-item']"
+    def self.gets_news
+      doc = gets_html(LINK)
+      content = gets_content(doc)
+      titles = gets_titles(content)
+    end
+    def self.gets_html(url)			
+      doc = Nokogiri::HTML(open(url))
+    end
+    def self.gets_content(doc)
+      news = doc.xpath(NEWS_XPATH)
+    end
+    def self.gets_titles(news)
+      news.map { |x| x.text }
+    end
+  end
 end
